fe7ca6f45a4da040c68a108b491747639b8c5d13
AlbertOR0994 <alberto.ocete.rosario@080formacion.alzago.es>
2024-04-05 14:06:38 +0100

    LOGIN settings

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './Login.css'
 import { login } from '../../services/auth'
 import "@fontsource/poppins"
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { LockOutlined, MailOutline } from "@mui/icons-material";
 
 function Login() {
