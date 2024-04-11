b08eebc4398b6422508cfcc744c76852c6c83838
AlbertOR0094 <alberto.ocete.rosario@080formacion.alzago.es>
2024-04-11 06:48:15 +0100

    -meds component

--- src/components/Urgency/Urgency.jsx
+++ src/components/Urgency/Urgency.jsx
@@ -1,28 +1,10 @@
-import { useState } from 'react'
-import { getAllMeds } from '../../services/meds'
-import { getAllAppointments } from '../../services/appointments'
-import { getAllReminders } from '../../services/reminder'
+import GetMeds from '../getmeds/GetMeds'
 
 const Urgency = () => {
-    const [medlist, setMedlist] = useState([])
-    const [reminderlist, setReminderlist] = useState([])
-    const [appointmentlist, setAppointmentlist] = useState([])
-
-    const getAllList = async () => {
-
-        setMedlist(await getAllMeds())
-        setReminderlist(await getAllReminders())
-        setAppointmentlist(await getAllAppointments())
-
-    }
-
+    
     return (
         <>
-            <div className='Cuadrante'>
-                <ul>
-                    <li></li>
-                </ul>
-            </div>
+            {GetMeds()}
         </>
     )
 }
